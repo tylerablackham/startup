76eff9be27f4e212b339350827bf59ab3b49a8c0
Tyler Blackham <tylerablackham@gmail.com>
2024-11-16 16:41:14 -0700

    fix: type module

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from "./components/Header.jsx"
 import Footer from "./components/Footer.jsx"
 import Login from "./login/Login.jsx"
-import {Navigate, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom"
 import Register from "./register/Register.jsx"
 import Transfers from "./transfers/Transfers.jsx"
 import Accounts from "./accounts/Accounts.jsx"
@@ -22,21 +22,23 @@ function App() {
     }
 
     return (
-        <div>
-            <Header isLoggedIn={isLoggedIn} username={username}/>
-            <main>
-                <Routes>
-                    <Route path="/login" element={<Login onLogin={handleAuthenticated} />} />
-                    <Route path="/register" element={<Register onLogin={handleAuthenticated} />} />
-                    <Route path="/transfers" element={isLoggedIn ? <Transfers /> : <Navigate to="/login" />} />
-                    <Route path="/accounts" element={isLoggedIn ? <Accounts /> : <Navigate to="/login" />} />
-                    <Route path="/logout" element={<Logout onLogout={handleUnauthenticated} />} />
-                    <Route path="/" element={<Navigate to="/login" />}/>
-                    <Route path="*" element={<Navigate to="/login" />}/>
-                </Routes>
-            </main>
-            <Footer/>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Header isLoggedIn={isLoggedIn} username={username}/>
+                <main>
+                    <Routes>
+                        <Route path="/login" element={<Login onLogin={handleAuthenticated} />} />
+                        <Route path="/register" element={<Register onLogin={handleAuthenticated} />} />
+                        <Route path="/transfers" element={isLoggedIn ? <Transfers /> : <Navigate to="/login" />} />
+                        <Route path="/accounts" element={isLoggedIn ? <Accounts /> : <Navigate to="/login" />} />
+                        <Route path="/logout" element={<Logout onLogout={handleUnauthenticated} />} />
+                        <Route path="/" element={<Navigate to="/login" />}/>
+                        <Route path="*" element={<Navigate to="/login" />}/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </div>
+        </BrowserRouter>
     )
 }
 
